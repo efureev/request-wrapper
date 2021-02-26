c79a0b1e0b7825be2a56a0a2174470088546eb69
Jack <furegin@yandex.ru>
2021-02-26 12:29:37 +0500

    add some fixes

--- src/interceptors/response/WrapperInterceptor/index.js
+++ src/interceptors/response/WrapperInterceptor/index.js
@@ -3,9 +3,11 @@ import WrapperInterceptor from './WrapperInterceptor'
 const wrapper = (config = {}) => ({
   dataKey: 'dataKey' in config ? config.dataKey : 'data',
   statusKey: config.statusKey ?? 'status',
-  fn: config.fn ?? ((instance) => {
-    instance.registerResponseInterceptors(WrapperInterceptor)
-  }),
+  fn:
+    config.fn ??
+    (instance => {
+      instance.registerResponseInterceptors(WrapperInterceptor)
+    }),
 })
 
 export default wrapper
